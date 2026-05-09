4018a427b86bfe1174f162cc0eec018d374b7316
Claycuy <unggaslauncher@gmail.com>
2026-05-09 20:04:49 +0800

    copyright: add copyright headwr

--- rust/src/instructions/math/mod/mod_f16in.rs
+++ rust/src/instructions/math/mod/mod_f16in.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 use half::f16;
 pub fn mod_f16in(a: u16, b: u16) -> u16 {
   let val_a = f16::from_bits(a);
