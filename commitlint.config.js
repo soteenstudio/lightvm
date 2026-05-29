025e6e60df31bd2cf55e60e23cb7aca7025694c2
Claycuy <unggaslauncher@gmail.com>
2026-05-29 13:57:59 +0800

    chore: Changed the rust code edition to 2024

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-min-length': [2, 'always', 10],
-    'subject-case': [2, 'always', 'sentance-case']
+    'subject-case': [2, 'always', 'sentence-case']
   }
 };
