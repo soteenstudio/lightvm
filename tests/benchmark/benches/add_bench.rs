856f4dc8cbb641c38e0be4b21c98ab85b96996ea
Claycuy <unggaslauncher@gmail.com>
2026-05-30 17:11:28 +0800

    chore: Add benchmark

---
+++ tests/benchmark/benches/add_bench.rs
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use lightvm::{LightVM, types::capability::Capability};
+fn bench_vm_execution(c: &mut Criterion) {
+  let mut vm = LightVM::new(vec![Capability::Control, Capability::Observe]);
+  let raw = serde_json::json!([
+    ["val", "x"],
+    ["push", 5],
+    ["push", 8],
+    ["add", "i16"],
+    ["set", "x"]
+  ]);
+  let optimized_json = LightVM::tools().optimize_bytecode(raw);
+  vm.load(optimized_json.clone());
+  let mut group = c.benchmark_group("LightVM Execution");
+  group.bench_function("add_bench", |b| {
+    b.iter(|| {
+      vm.run(None)
+    });
+  });
+  group.finish();
+}
+criterion_group!(benches, bench_vm_execution);
+criterion_main!(benches);
---
+++ tests/benchmark/benches/io_bench.rs
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use lightvm::{LightVM, types::capability::Capability};
+fn bench_vm_execution(c: &mut Criterion) {
+  let mut vm = LightVM::new(vec![Capability::Control, Capability::Observe]);
+  let raw = serde_json::json!([["push", "Hello from LightVM!"], ["println"]]);
+  let optimized_json = LightVM::tools().optimize_bytecode(raw);
+  vm.load(optimized_json.clone());
+  let mut group = c.benchmark_group("LightVM Execution");
+  group.bench_function("io_bench", |b| {
+    b.iter(|| {
+      let _sink = std::io::sink();
+      vm.run(None)
+    });
+  });
+  group.finish();
+}
+criterion_group!(benches, bench_vm_execution);
+criterion_main!(benches);
