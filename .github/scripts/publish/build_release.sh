72daad19d0de78d3500f15758f205748628be510
Claycuy <unggaslauncher@gmail.com>
2026-07-05 13:27:19 +0800

    fix(publish): Env variables

--- .github/scripts/publish/build_release.sh
+++ .github/scripts/publish/build_release.sh
@@ -1,15 +1,15 @@
-if [[ "${{ matrix.platform }}" == *"android"* ]]; then
-  cargo build --release --target ${{ matrix.target }}
-elif [[ "${{ matrix.platform }}" == *"musl"* ]]; then
+if [[ "$PLATFORM" == *"android"* ]]; then
+  cargo build --release --target $TARGET
+elif [[ "$PLATFORM" == *"musl"* ]]; then
   if [ ! -f "./cross" ]; then
     curl -L https://github.com/cross-rs/cross/releases/latest/download/cross-x86_64-unknown-linux-musl.tar.gz | tar xz
     chmod +x cross
   fi
-  RUSTFLAGS="-C target-feature=-crt-static" ./cross build --release --target ${{ matrix.target }}
+  RUSTFLAGS="-C target-feature=-crt-static" ./cross build --release --target $TARGET
 else
   if [ ! -f "./cross" ]; then
     curl -L https://github.com/cross-rs/cross/releases/latest/download/cross-x86_64-unknown-linux-musl.tar.gz | tar xz
     chmod +x cross
   fi
-  ./cross build --release --target ${{ matrix.target }}
+  ./cross build --release --target $TARGET
 fi
