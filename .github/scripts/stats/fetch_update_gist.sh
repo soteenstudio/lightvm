97aca40b3840c516a2c6c1e810321a17de775b50
Claycuy <unggaslauncher@gmail.com>
2026-07-05 13:22:38 +0800

    fix(stats): Env variables

--- .github/scripts/stats/fetch_update_gist.sh
+++ .github/scripts/stats/fetch_update_gist.sh
@@ -1,14 +1,14 @@
-CLONES=$(curl -s -H "Authorization: token ${{ secrets.LIGHTVM_PAT }}" \
-"https://api.github.com/repos/${{ github.repository }}/traffic/clones" \
+CLONES=$(curl -s -H "Authorization: token $LIGHTVM_PAT" \
+"https://api.github.com/repos/$GITHUB_REPOSITORY/traffic/clones" \
 | jq '.count // 0')
 
-TRAFFIC_DATA=$(curl -s -H "Authorization: token ${{ secrets.LIGHTVM_PAT }}" \
-"https://api.github.com/repos/${{ github.repository }}/traffic/views")
+TRAFFIC_DATA=$(curl -s -H "Authorization: token $LIGHTVM_PAT" \
+"https://api.github.com/repos/$GITHUB_REPOSITORY/traffic/views")
 
 TOTAL_VIEWS=$(echo $TRAFFIC_DATA | jq '.count // 0')
 UNIQUE_VISITORS=$(echo $TRAFFIC_DATA | jq '.uniques // 0')
 
-curl -s --fail -X PATCH -H "Authorization: token ${{ secrets.LIGHTVM_PAT }}" \
+curl -s --fail -X PATCH -H "Authorization: token $LIGHTVM_PAT" \
 -H "Content-Type: application/json" \
 -d "{\"files\":{\"stats.json\":{\"content\":\"{\\\"clones\\\": $CLONES, \\\"total_views\\\": $TOTAL_VIEWS, \\\"unique_visitors\\\": $UNIQUE_VISITORS}\"}}}" \
-"https://api.github.com/gists/${{ secrets.GIST_ID }}"
+"https://api.github.com/gists/$GIST_ID"
