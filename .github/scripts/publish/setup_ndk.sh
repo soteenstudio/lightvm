0401372ecd5e9ef8e3e5c07e669a5b40214c25ea
Claycuy <unggaslauncher@gmail.com>
2026-07-14 09:11:29 +0800

    ci: Fix setup ndk

--- .github/scripts/publish/setup_ndk.sh
+++ .github/scripts/publish/setup_ndk.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
-
-if [ -z "$ANDROID_NDK_HOME" ]; then
-  export ANDROID_NDK_HOME=$ANDROID_NDK_LATEST_HOME
+if [ -z "$ANDROID_NDK_LATEST_HOME" ]; then
+  export ANDROID_NDK_LATEST_HOME=$(ls -d /usr/local/lib/android/sdk/ndk/* | tail -1)
 fi
 
-echo "Use NDK from: $ANDROID_NDK_HOME"
+NDK_PATH=${ANDROID_NDK_LATEST_HOME}
+TOOLCHAIN="$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/bin"
+
+echo "ANDROID_NDK_HOME=$ANDROID_NDK_LATEST_HOME" >> $GITHUB_ENV
+
+TOOLCHAIN="$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin"
+
+mkdir -p .cargo
+if [ "${{ matrix.platform }}" == "android-arm64" ]; then
+  LINKER="aarch64-linux-android33-clang"
+  echo "CC=$TOOLCHAIN/aarch64-linux-android33-clang" >> $GITHUB_ENV
+else
+  LINKER="armv7a-linux-androideabi33-clang"
+  echo "CC=$TOOLCHAIN/armv7a-linux-androideabi33-clang" >> $GITHUB_ENV
+fi
+
+echo "AR=$TOOLCHAIN/llvm-ar" >> $GITHUB_ENV
+
+echo "[target.${{ matrix.target }}]" > .cargo/config.toml
+echo "ar = \"$TOOLCHAIN/llvm-ar\"" >> .cargo/config.toml
+echo "linker = \"$TOOLCHAIN/$LINKER\"" >> .cargo/config.toml
