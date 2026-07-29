5bd0ba3ad2beb6e8ea09fda3831f806bbb9d35a8
Claycuy <unggaslauncher@gmail.com>
2026-07-30 06:04:04 +0800

    ci(nightly): Fix commit push

--- .github/scripts/nightly/create_release.sh
+++ .github/scripts/nightly/create_release.sh
@@ -1,12 +1,15 @@
 PREV_TAG=$(git tag --sort=-creatordate | sed -n '2p')
 
 if [ -z "$PREV_TAG" ]; then
+  echo "PREV_TAG not found. Cannot create release notes because this is the first build."
+  exit 1
+fi
+
+if [ -z "$PREV_TAG" ]; then
   LOGS=$(git log --pretty=format:"%s")
 else
   LOGS=$(git log $PREV_TAG..HEAD --pretty=format:"%s")
 fi
-
-LOGS=$(git log $PREV_TAG..HEAD --pretty=format:"%s")
 
 FEAT=$(echo "$LOGS" | grep -E "^feat(\(.*\))?: " | sed -E 's/^feat(\(.*\))?: /- /' || echo "")
 FIX=$(echo "$LOGS" | grep -E "^fix(\(.*\))?: " | sed -E 's/^fix(\(.*\))?: /- /' || echo "")
