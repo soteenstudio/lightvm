dc8d6db9a799bd0408d34d0343fa99c96baeef47
Claycuy <unggaslauncher@gmail.com>
2026-07-30 07:27:52 +0800

    ci(nightly): Fix commit push

--- .github/scripts/nightly/commit_push.sh
+++ .github/scripts/nightly/commit_push.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-: "${VERSION_VAL:?VERSION_VAL must be set}"
-
+# 1. Setup Identity
 git config user.name "github-actions[bot]"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-
 git config commit.gpgsign true
 git config tag.gpgsign true
 
-export GIT_EDITOR=true
-git fetch --no-recurse-submodules --depth=1 origin
+git fetch origin main
 
-git add Cargo.toml
-[ -f "package.json" ] && git add package.json
 git add -A
-
 if git diff --cached --quiet; then
   echo "No file changes, skip push."
   exit 0
 fi
+git commit -m "chore: nightly release $VERSION_VAL"
 
-git commit -m "chore: nightly release ${VERSION_VAL}"
+git rebase origin/main
 
 git tag -a "$VERSION_VAL" -m "Nightly Release $VERSION_VAL"
-git push origin HEAD --tags --no-verify
+git push origin HEAD:main --follow-tags --no-verify
