30e24f12ccf31dff9ef68fb66e8d65248ef1bb3a
Claycuy <unggaslauncher@gmail.com>
2026-08-06 13:35:19 +0800

    feat(llibsc): Update io.c libs

--- libs/io.c
+++ libs/io.c
@@ -1,18 +1,18 @@
 /*
-* Copyright 2025-2026 SoTeen Studio
-*
-* Licensed under the Apache License, Version 2.0 (the "License")
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*/
+ * Copyright 2025-2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 #include <stdio.h>
 
 typedef struct {
-  long long type_tag; // 0 = Int, 1 = Bool, 2 = Float, dst.
-  long long value;    // Nilai aslinya
+  long long type_tag; // 0 = Int, 1 = Bool, 2 = Float, 3 = String, dst.
+  long long value;    // Nilai aslinya atau pointer string di .rodata
 } LightVMValue;
 
 void lightvm_print(LightVMValue* val_ptr) {
@@ -21,8 +21,14 @@ void lightvm_print(LightVMValue* val_ptr) {
   if (val_ptr->type_tag == 1) {
     // Tag 1 adalah Boolean
     printf("%s", val_ptr->value ? "true" : "false");
+  } else if (val_ptr->type_tag == 2) {
+    // Tag 2 adalah Float (jika nilainya di-cast dari double/float representation)
+    printf("%f", *(double*)&val_ptr->value);
+  } else if (val_ptr->type_tag == 3) {
+    // Tag 3 adalah String (value adalah pointer char*)
+    printf("%s", (char*)val_ptr->value);
   } else {
-    // Tag 0 atau lainnya dicetak sebagai angka biasa
+    // Tag 0 atau lainnya dicetak sebagai integer biasa
     printf("%lld", val_ptr->value);
   }
 }
