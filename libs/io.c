62504fd550e95e482d034b2ba936c2a48586c09c
Claycuy <unggaslauncher@gmail.com>
2026-08-07 21:40:53 +0800

    chore: Tidying up LLibsC code

--- libs/io.c
+++ libs/io.c
@@ -10,32 +10,41 @@
 
 #include <stdio.h>
 
-// ANSI Color Codes
+// These colors are provided to make the display on the CLI more readable.
+// - Green -> Used for number colors (eg: integer, float, etc.)
+// - Megenta or purple -> Used for boolean colors (eg: true and false)
+// - Cyan or light blue -> Used for object and array colors
+// - Yellow -> Used for string colors
 #define COLOR_RESET   "\033[0m"
-#define COLOR_INT     "\033[32m" // Hijau
-#define COLOR_BOOL    "\033[35m" // Magenta / Ungu
-#define COLOR_FLOAT   "\033[36m" // Cyan / Biru Muda
-#define COLOR_STRING  "\033[33m" // Kuning
+#define COLOR_INT     "\033[32m"
+#define COLOR_BOOL    "\033[35m"
+#define COLOR_OBJ   "\033[36m"
+#define COLOR_STRING  "\033[33m"
 
 typedef struct {
-  long long type_tag; // 0 = Int, 1 = Bool, 2 = Float, 3 = String, dst.
-  long long value;    // Nilai aslinya atau pointer string di .rodata
+  long long type_tag;
+  long long value;    // Original value or string pointer in .rodata
 } LightVMValue;
 
 void lightvm_print(LightVMValue* val_ptr) {
   if (!val_ptr) return;
-
-  if (val_ptr->type_tag == 1) {
-    // Tag 1 adalah Boolean (Ungu)
+  
+  // 0 -> Tag for Fallback
+  // 1 -> Tag for Boolean
+  // 2 -> Tag for Number
+  // 3 -> Tag for Object/Array
+  // 4 -> Tag for String
+  if (val_ptr->type_tag == 0) {
+    printf("%s%lld%s", COLOR_INT, val_ptr->value, COLOR_RESET);
+  } else if (val_ptr->type_tag == 1) {
     printf("%s%s%s", COLOR_BOOL, val_ptr->value ? "true" : "false", COLOR_RESET);
   } else if (val_ptr->type_tag == 2) {
-    // Tag 2 adalah Float (Cyan)
-    printf("%s%f%s", COLOR_FLOAT, *(double*)&val_ptr->value, COLOR_RESET);
+    printf("%s%f%s", COLOR_INT, *(double*)&val_ptr->value, COLOR_RESET);
   } else if (val_ptr->type_tag == 3) {
-    // Tag 3 adalah String (Kuning)
+    printf("%s%s%s", COLOR_OBJ, (char*)val_ptr->value, COLOR_RESET);
+  } else if (val_ptr->type_tag == 4) {
     printf("%s%s%s", COLOR_STRING, (char*)val_ptr->value, COLOR_RESET);
   } else {
-    // Tag 0 atau lainnya dicetak sebagai integer biasa (Hijau)
     printf("%s%lld%s", COLOR_INT, val_ptr->value, COLOR_RESET);
   }
 }
