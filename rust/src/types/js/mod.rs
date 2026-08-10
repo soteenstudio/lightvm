b356f65a4ee91862278b9b769895a668ecd45e17
Claycuy <unggaslauncher@gmail.com>
2026-08-10 08:36:00 +0800

    chore: Added copyright header

--- rust/src/types/js/mod.rs
+++ rust/src/types/js/mod.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2025-2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 pub(crate) mod js_error_options;
 pub(crate) mod js_runtime_config;
 pub(crate) mod js_security_config;
