b93eab3d09aa0824ac2434b04e58bc56b8505201
Claycuy <unggaslauncher@gmail.com>
2026-08-21 07:27:07 +0800

    chore: Added specific script for test-install

---
+++ .testings/test-install/napi.js
@@ -0,0 +1,23 @@
+import { LightVM, Capability } from 'lightvm';
+
+function main() {
+  const vm = new LightVM({
+    caps: [Capability.Observe, Capability.Control],
+  });
+  const tools = vm.tools();
+  const raw = [
+    ['jump', 7],
+    ['func', 'add', 2, 2, 6, 'a', 'b'],
+    ['get', 'a'],
+    ['get', 'b'],
+    ['add', 'int'],
+    ['return'],
+    ['stop'],
+    ['export', 'add'],
+  ];
+  const optimized = tools.optimizeBytecode(raw);
+  vm.load(optimized);
+  const addFunc = vm.export('add');
+  console.log(addFunc(5, 6));
+}
+main();
