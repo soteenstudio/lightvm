15018f2763c07ec3bb3b7cc103eb97139963618d
Claycuy <unggaslauncher@gmail.com>
2026-08-29 06:47:35 +0800

    feat: Update color to Dying

--- rust/src/modules/versions.rs
+++ rust/src/modules/versions.rs
@@ -8,13 +8,8 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
+use crate::modules::dying::colors::*;
 use std::fmt;
-pub const RED: &str = "\x1b[31;1m";
-pub const _YELLOW: &str = "\x1b[33m";
-pub const CYAN: &str = "\x1b[36m";
-pub const DARK_GRAY: &str = "\x1b[2;37m";
-pub const RESET: &str = "\x1b[0m";
-pub const BOLD: &str = "\x1b[1m";
 #[derive(Debug)]
 struct ModuleVersions {
   carzy: String,
