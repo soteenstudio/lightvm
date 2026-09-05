f3318a057d414df64d15f24ccd214ed7bcbb3cd8
claycuy <unggaslauncher@gmail.com>
2026-09-06 00:21:32 +0800

    refactor: Cleanup source code

--- rust/src/instructions/math/vector/trigonometry/inverse/asinv/mod.rs
+++ rust/src/instructions/math/vector/trigonometry/inverse/asinv/mod.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2025-2026 SoTeen Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 pub(crate) mod asinv_f16in;
 pub(crate) mod asinv_f32in;
 pub(crate) mod asinv_f64in;
